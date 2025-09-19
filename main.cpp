65cf12e4e968fa2e7eb283b5aa88fd483f5aa6bf
Taylor Bahr <tbahr00@gmail.com>
2025-09-19 17:32:39 -0500

    Add files

---
+++ main.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+
+// Sample comment 1
+// Sample comment 2
+
+// Sample 3
+
+int main() {
+	std::cout << "This is a sample main file\n";
+	return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,8 @@
+
+#include <gtest/gtest.h>
+
+
+
+TEST(SampleTests, Test) {
+	EXPECT_EQ(1, 1);
+}
